63e44c2d1c5e1b74a48b29ab58da1d2153a0c99f
FernandoCaccharelli <fer.caccharelli@gmail.com>
2021-11-15 20:32:06 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+
+app.listen(3000, ()=>{
+    console.log('Servidor funcionando');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/views/login.html', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/views/register.html', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.post('/register', (req,res) =>{
+    res.redirect ("/");
+});
+
+app.post('/login', (req,res) =>{
+    res.redirect ("/");
+});
